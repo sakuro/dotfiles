9d43c39b36e526113319968f053c9e80849c11e3
OZAWA Sakuro <sakuro@2238.club>
2019-04-11 20:18:56 +0900

    Remove rbenv stuff

--- rbenv/hooks/install/install_or_update_bundler.bash
+++
@@ -1,2 +0,0 @@
-after_install 'export RBENV_VERSION="$VERSION_NAME"; \
-gem list | grep -q bundler && gem update bundler || gem install bundler'
--- rbenv/hooks/install/update_rubygems.bash
+++
@@ -1,4 +0,0 @@
-after_install 'export RBENV_VERSION="$VERSION_NAME"; \
-gem update --system; \
-gem uninstall rubygems-update --all --executables; \
-gem -v'
