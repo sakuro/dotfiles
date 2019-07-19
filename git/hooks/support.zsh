36a3c25fc0ce3890398a2d966e4a709f8e2d6c70
OZAWA Sakuro <sakuro@2238.club>
2019-07-19 10:53:49 +0900

    Update git hooks

---
+++ git/hooks/support.zsh
@@ -0,0 +1,10 @@
+function detect_languages() {
+    [[ -f Gemfile ]] && echo ruby || :
+    [[ -f package.json ]] && echo nodejs || :
+    return 0
+}
+
+
+function command-in-path() {
+  whence -p $* > /dev/null
+}
