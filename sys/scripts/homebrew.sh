4e7de9cb760f1af278343e62ec2f69762f585b07
OZAWA Sakuro <sakuro@2238.club>
2019-09-29 00:55:02 +0900

    Fix path and function names

--- sys/scripts/homebrew.sh
+++ sys/scripts/homebrew.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-. $(dirname $0)/defs.sh
+. $(dirname $0)/../dotfiles.sh
 
 BREW_INSTALLER_URL=https://raw.githubusercontent.com/Homebrew/install/master/install
 BREW_ORIGINAL_ROOT=/usr/local
 BREW_ROOT=/opt/brew
 
-if is_executable brew; then
+if is-executable brew; then
   echo "Homebrew is already installed"
   exit 0
 else
