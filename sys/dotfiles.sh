c4e37a8bd4bb994e6b051c08f6f048fde3af35d8
OZAWA Sakuro <sakuro@2238.club>
2019-09-29 13:12:51 +0900

    Fix install test

--- sys/dotfiles.sh
+++ sys/dotfiles.sh
@@ -53,7 +53,7 @@ is-executable() {
 
 function macos::prepare() { macos::clt::should-install && macos::clt::install; }
 function macos::os::version() { sw_vers | grep ProuctVersion | grep -Eo '10\.[0-9]+'; }
-function macos::clt::should-install() { [[ -e /Library/Developer/CommandLineTools/usr/bin/git ]]; }
+function macos::clt::should-install() { [[ ! -e /Library/Developer/CommandLineTools/usr/bin/git ]]; }
 function macos::clt::install() {
   sudo /usr/bin/xcode-select --install
   echo Press any key when the installation has completed
