d917efdb0c21cf364510b0e2321b1f9fb94c715d
OZAWA Sakuro <sakuro@2238.club>
2019-09-29 21:51:20 +0900

    Install java

--- sys/scripts/homebrew.sh
+++ sys/scripts/homebrew.sh
@@ -4,11 +4,13 @@
 
 BREW_INSTALLER_URL=https://raw.githubusercontent.com/Homebrew/install/master/install
 BREW_ORIGINAL_ROOT=/usr/local
-BREW_ROOT=/opt/brew
 
 if is-executable brew; then
   echo "Homebrew is already installed"
   exit 0
 else
   /usr/bin/ruby -e "$(curl -fsSL "${BREW_INSTALLER_URL}" | sed -e "s!${BREW_ORIGINAL_ROOT}!${BREW_ROOT}!g")"
+  # Some formula requires java cask but casks are listed after formulae, so this must be installed beforehand
+  brew update
+  brew cask install java
 fi
