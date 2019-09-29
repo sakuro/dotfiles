0c20026f1b3f516227a56d6bd0303d81e599252f
OZAWA Sakuro <sakuro@2238.club>
2019-09-29 23:15:11 +0900

    Update comment

--- sys/scripts/homebrew.sh
+++ sys/scripts/homebrew.sh
@@ -13,8 +13,8 @@ else
   /usr/bin/ruby -e "$(curl -fsSL "${BREW_INSTALLER_URL}" | sed -e "s!${BREW_ORIGINAL_ROOT}!${BREW_ROOT}!g")"
 fi
 
-# Some formula requires java cask but casks are listed after formulae,
-# so this must be installed beforehand
+# Some formulae require java cask be installed, but in Brewfile, casks are
+# listed after formulae, so this must be installed beforehand
 if /usr/libexec/java_home >/dev/null 2>/dev/null; then
   :
 else
