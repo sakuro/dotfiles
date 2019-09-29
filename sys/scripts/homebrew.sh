d561e879a9fb880c700768235642aa33f823a41b
OZAWA Sakuro <sakuro@2238.club>
2019-09-29 23:13:26 +0900

    Force java install

--- sys/scripts/homebrew.sh
+++ sys/scripts/homebrew.sh
@@ -11,14 +11,15 @@ if is-executable brew; then
   echo "Homebrew is already installed"
 else
   /usr/bin/ruby -e "$(curl -fsSL "${BREW_INSTALLER_URL}" | sed -e "s!${BREW_ORIGINAL_ROOT}!${BREW_ROOT}!g")"
-  if /usr/libexec/java_home >/dev/null 2>/dev/null; then
-    :
-  else
-    # Some formula requires java cask but casks are listed after formulae,
-    # so this must be installed beforehand
-    brew update
-    brew cask install java
-  fi
+fi
+
+# Some formula requires java cask but casks are listed after formulae,
+# so this must be installed beforehand
+if /usr/libexec/java_home >/dev/null 2>/dev/null; then
+  :
+else
+  brew update
+  brew cask install java
 fi
 
 brew bundle
