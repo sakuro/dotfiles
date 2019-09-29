32b21328e0e26054e15f892b290cdd3511779cdf
OZAWA Sakuro <sakuro@2238.club>
2019-09-29 22:11:21 +0900

    Check java_home before installing java

--- sys/scripts/homebrew.sh
+++ sys/scripts/homebrew.sh
@@ -10,7 +10,12 @@ if is-executable brew; then
   exit 0
 else
   /usr/bin/ruby -e "$(curl -fsSL "${BREW_INSTALLER_URL}" | sed -e "s!${BREW_ORIGINAL_ROOT}!${BREW_ROOT}!g")"
-  # Some formula requires java cask but casks are listed after formulae, so this must be installed beforehand
-  brew update
-  brew cask install java
+  if [[ /usr/libexec/java_home >/dev/null 2>/dev/null ]]; then
+    :
+  else
+    # Some formula requires java cask but casks are listed after formulae,
+    # so this must be installed beforehand
+    brew update
+    brew cask install java
+  fi
 fi
