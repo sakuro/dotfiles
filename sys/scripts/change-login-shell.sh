9de8d631e5cded8d23babac9a33b8cd75054ae5a
OZAWA Sakuro <sakuro@2238.club>
2021-11-16 17:05:08 +0900

    Use bash

--- sys/scripts/change-login-shell.sh
+++ sys/scripts/change-login-shell.sh
@@ -1,20 +1,37 @@
-#!/bin/sh
+#!/bin/bash
 
 LOGIN_SHELL=${LOGIN_SHELL:=zsh}
 
-# if multiple paths are found, use the last one
-local shell_path="$(grep "/${LOGIN_SHELL}"'$' /etc/shells | sed '$!d')"
-if [[ -z "${shell_path}" ]]; then
-  echo "${LOGIN_SHELL} could not be found in /etc/shells"
+set -- $(grep "/$LOGIN_SHELL"'$' /etc/shells)
+case $# in
+0)
+  echo "$LOGIN_SHELL could not be found in /etc/shells"
+  exit 1
+  ;;
+1)
+  shell_path=$1
+  ;;
+*)
+  select shell_path; do
+    break
+  done
+  ;;
+esac
+
+case $OSTYPE in
+darwin*)
+  set -- $(dscl localhost -read Local/Default/Users/$USER UserShell)
+  current_shell=$2
+  ;;
+*)
+  echo "Unsupported OS: $OSTYPE"
+  ;;
+esac
+
+if [[ ${shell_path##*/} = ${current_shell##*/} ]]; then
+  echo "The login shell is already $current_shell"
   exit 0
 fi
 
-set -- $(dscl localhost -read Local/Default/Users/$USER UserShell)
-local current_shell=$2
-if [[ $shell_path = $current_shell ]]; then
-  echo "The login shell is already ${current_shell}"
-  exit 0
-fi
-
-# using sudo incase the user's password is not known
-sudo chsh -s "${shell_path}" "$(id -un)"
+# using sudo in case the user's password is not known
+sudo chsh -s ${shell_path} $(id -un)
