828af5a403388f4eabf3900a2d90763e36b5a81c
OZAWA Sakuro <sakuro@2238.club>
2021-11-16 10:33:42 +0900

    Install mas

--- sys/scripts/install-homebrew.sh
+++ sys/scripts/install-homebrew.sh
@@ -3,4 +3,12 @@
 PATH=
 eval "$(/usr/libexec/path_helper)"
 type -P brew > /dev/null && exit 0
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+type -P mas > /dev/null || brew install mas
+mas_account="$(mas account)"
+until [[ $? = 0 ]]; do
+  echo "Log in to the AppStore and press any key" && read answer < /dev/tty
+  mas_account="$(mas account)"
+done
+echo "Using the AppStore account: ${mas_account}"
