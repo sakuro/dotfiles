80ac33c0023c01d2e6bbe44975daed7fceced990
OZAWA Sakuro <sakuro@2238.club>
2021-11-16 22:41:40 +0900

    Install plugin and tool

--- sys/scripts/install-asdf.sh
+++ sys/scripts/install-asdf.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-TOOL_VERSIONS_FILE="${HOME}/.tool-versions"
-PLUGINS=( $(awk '{print $1}' "${TOOL_VERSIONS_FILE}") )
+TOOL_VERSIONS_FILE=$HOME/.tool-versions
 
-type -P asdf > /dev/null && exit 0
-git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf
+type -P asdf > /dev/null || git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf
 
-if [[ -f "${TOOL_VERSIONS_FILE}" ]]; then
+if [[ -f $TOOL_VERSIONS_FILE ]]; then
   source $HOME/.asdf/asdf.sh
-  for plugin in "${PLUGINS[@]}"; do
-    echo "Installing plugin: ${plugin}"
-    asdf plugin-add "${plugin}"
-  done
+  while read plugin version; do
+    if asdf plugin list | grep -qxF $plugin; then
+      echo "Updating plugin: $plugin"
+      asdf plugin-update $plugin
+    else
+      echo "Installing plugin: $plugin"
+      asdf plugin-add $plugin
+    fi
+    if asdf list $plugin | grep -qxF $version; then
+      echo "$plugin $version is already installed"
+    else
+      echo "Installing $plugin $version"
+      asdf install $plugin $version
+    fi
+  done < $TOOL_VERSIONS_FILE
 else
   echo "${TOOL_VERSIONS_FILE/$HOME/~/} does not exist"
 fi
