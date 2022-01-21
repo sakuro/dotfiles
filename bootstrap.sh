a17a81e100e9024bd541b6d20954a7b47806911a
OZAWA Sakuro <sakuro@2238.club>
2022-01-21 10:21:41 +0900

    Fix waiting prompt

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ function bootstrap-macos()
 {
   if [[ ! -e /Library/Developer/CommandLineTools/usr/bin/git ]]; then
     sudo /usr/bin/xcode-select --install
-    echo -n "Press any key when the installation has completed"; read answer < /dev/tty
+    echo -n "Press ENTER when the installation has completed"; read answer < /dev/tty
     sudo /usr/bin/xcode-select --switch /Library/Developer/CommandLineTools
   fi
 }
