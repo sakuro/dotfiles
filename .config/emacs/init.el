33029e042a6487f4699ed06270e16acb87280c51
OZAWA Sakuro <sakuro@2238.club>
2022-01-27 18:19:56 +0900

    Use leaf.el

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,1 +1,21 @@
-(setq inhibit-startup-message t)
+(customize-set-variable
+ 'package-archives '(("gnu"   . "https://elpa.gnu.org/packages/")
+                     ("melpa" . "https://melpa.org/packages/")
+                     ("org"   . "https://orgmode.org/elpa/")))
+
+
+(package-initialize)
+(unless (package-installed-p 'leaf)
+  (package-refresh-contents)
+  (package-install 'leaf))
+
+(leaf leaf-keywords
+  :ensure t
+  :init
+  :config
+  (leaf-keywords-init))
+
+(customize-set-variable 'custom-file (locate-user-emacs-file "custom.el"))
+(customize-set-variable 'inhibit-startup-message t)
+
+(provide 'init)
