9bd584db13f09041d47dfbb364a77d71c800b5cb
OZAWA Sakuro <sakuro@2238.club>
2022-01-27 18:29:01 +0900

    Configure default frame

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -2,8 +2,6 @@
  'package-archives '(("gnu"   . "https://elpa.gnu.org/packages/")
                      ("melpa" . "https://melpa.org/packages/")
                      ("org"   . "https://orgmode.org/elpa/")))
-
-
 (package-initialize)
 (unless (package-installed-p 'leaf)
   (package-refresh-contents)
@@ -17,5 +15,12 @@
 
 (customize-set-variable 'custom-file (locate-user-emacs-file "custom.el"))
 (customize-set-variable 'inhibit-startup-message t)
+(customize-set-variable 'default-frame-alist
+    '(
+      (width . 120)
+      (height . 40)
+      (top . 0)
+      (left . 0)
+      (font . "-*-Cica-normal-normal-normal-*-16-*-*-*-m-0-iso10646-1")))
 
 (provide 'init)
