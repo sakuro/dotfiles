413c14694ca3290014dcccf1454af4497c0604e1
OZAWA Sakuro <sakuro@2238.club>
2022-02-01 17:23:42 +0900

    Alacritty is now multi-arch binary

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -5,11 +5,7 @@ darwin*)
     eval "$(/usr/libexec/path_helper)"
     PATH=/opt/homebrew/bin:$PATH
     type -P brew > /dev/null || scripts/install-homebrew.sh
-    if [[ $(uname -m) = *arm64* ]] && [[ $(arch) = i386 ]]; then
-        arch -arch arm64 brew bundle --no-lock
-    else
-        brew bundle --no-lock
-    fi
+    brew bundle --no-lock
     chmod go-w "$(brew --prefix)/share"
     ;;
 *)
