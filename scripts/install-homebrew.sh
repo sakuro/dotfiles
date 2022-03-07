171d34ef331d51dd5a07d0c3e89a61fea09fc513
OZAWA Sakuro <sakuro@2238.club>
2022-03-07 11:16:27 +0900

    mas doesn't run on Monterey

--- scripts/install-homebrew.sh
+++ scripts/install-homebrew.sh
@@ -6,10 +6,12 @@ type -P brew > /dev/null && exit 0
 bash -c "$(curl --fail --silent --show-error --location https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 type -P mas > /dev/null || brew install mas
-mas_account="$(mas account)"
-# shellcheck disable=SC2181
-until [[ $? = 0 ]]; do
-  echo "Log in to the AppStore and press ENTER" && read -r < /dev/tty
+if [[ "${$(sw_vers -productVersion)%%.*}" -lt 12 ]]; then
   mas_account="$(mas account)"
-done
-echo "Using the AppStore account: ${mas_account}"
+  # shellcheck disable=SC2181
+  until [[ $? = 0 ]]; do
+    echo "Log in to the AppStore and press ENTER" && read -r < /dev/tty
+    mas_account="$(mas account)"
+  done
+  echo "Using the AppStore account: ${mas_account}"
+fi
