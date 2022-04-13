40d96aff53c6332c2dea69223abd705bfe729a4b
OZAWA Sakuro <sakuro@2238.club>
2022-04-13 17:51:52 +0900

    Use temporary variable

--- scripts/install-homebrew.sh
+++ scripts/install-homebrew.sh
@@ -6,7 +6,8 @@ type -P brew > /dev/null && exit 0
 bash -c "$(curl --fail --silent --show-error --location https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 type -P mas > /dev/null || brew install mas
-if [[ "${$(sw_vers -productVersion)%%.*}" -lt 12 ]]; then
+ver=$(sw_vers -productVersion)
+if [[ "${ver%%.*}" -lt 12 ]]; then
   until mas_account="$(mas account)"; do
     echo "Log in to the AppStore and press ENTER" && read -r < /dev/tty
   done
