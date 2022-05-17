6497a24666e4ec82ff3407ec5f6934bdac9f03e7
OZAWA Sakuro <sakuro@2238.club>
2022-05-17 19:29:50 +0900

    Add debian

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -8,6 +8,24 @@ darwin*)
     brew bundle --no-lock
     chmod go-w "$(brew --prefix)/share"
     ;;
+*linux*)
+    [[ -f /etc/os-release ]] || {
+      echo "Unknown Linux (/etc/os-release does not exist)"
+      exit 1
+    }
+    # shellcheck disable=SC1091
+    source /etc/os-release
+
+    case $ID in
+    debian)
+      apt install tmux
+      ;;
+    *)
+      echo "Unsupported Linux: $ID / $PRETTY_NAME"
+      exit 1
+      ;;
+    esac
+    ;;
 *)
     echo "Unsupported OS: $OSTYPE"
     exit 1
