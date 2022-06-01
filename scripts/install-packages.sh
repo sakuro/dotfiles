77d005cace7b184968e86ad1d8051cc5c3f01ef3
OZAWA Sakuro <sakuro@2238.club>
2022-06-01 14:40:36 +0900

    Install apt packages

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+function install_source_list()
+{
+    # GitHub CLI
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+}
+
 case "${OSTYPE}" in
 darwin*)
     eval "$(/usr/libexec/path_helper)"
@@ -18,8 +25,9 @@ darwin*)
 
     case $ID in
     debian)
-      sudo apt
-      sudo apt install --yes less tmux vim zsh
+      install_source_list
+      sudo apt update
+      sudo apt install --yes $(cat apt-packages.txt)
       ;;
     *)
       echo "Unsupported Linux: $ID / $PRETTY_NAME"
