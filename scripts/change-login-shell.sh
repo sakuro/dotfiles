8ba9a608b1287f45deaa625e2840741a8225ff24
OZAWA Sakuro <sakuro@2238.club>
2022-07-20 16:01:41 +0900

    Extract current_shell function

--- scripts/change-login-shell.sh
+++ scripts/change-login-shell.sh
@@ -2,6 +2,22 @@
 
 LOGIN_SHELL=${LOGIN_SHELL:=zsh}
 
+function current_shell()
+{
+  case "$OSTYPE" in
+  darwin*)
+    # shellcheck disable=SC2046
+    set -- $(dscl localhost -read "Local/Default/Users/$USER" UserShell)
+    echo "$2"
+    ;;
+  *)
+    # shellcheck disable=SC2046
+    set -- $(getent passwd "$USER" | cut --delimiter=: --fields=7)
+    echo "$1"
+    ;;
+  esac
+}
+
 # shellcheck disable=SC2046
 set -- $(grep "/$LOGIN_SHELL"'$' /etc/shells)
 case $# in
@@ -21,19 +37,7 @@ esac
 
 : "${USER:=$(id --user --name)}"
 
-case "$OSTYPE" in
-darwin*)
-  # shellcheck disable=SC2046
-  set -- $(dscl localhost -read "Local/Default/Users/$USER" UserShell)
-  current_shell=$2
-  ;;
-*)
-  # shellcheck disable=SC2046
-  set -- $(getent passwd "$USER" | cut --delimiter=: --fields=7)
-  current_shell=$1
-  ;;
-esac
-
+current_shell=$(current_shell)
 if [[ "${shell_path##*/}" = "${current_shell##*/}" ]]; then
   echo "The login shell is already $current_shell"
   exit 0
@@ -41,3 +45,4 @@ fi
 
 # using sudo in case the user's password is not known
 sudo chsh -s "${shell_path}" "$USER"
+echo "The login shell is set to $(current_shell)"
