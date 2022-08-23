4f84b04307e9a51b117992ad8ac4663cc347015e
OZAWA Sakuro <sakuro@2238.club>
2022-08-23 09:37:14 +0900

    Use os detection util

--- scripts/install-locale.sh
+++ scripts/install-locale.sh
@@ -1,30 +1,17 @@
 #!/bin/bash
 
-case "${OSTYPE}" in
-darwin*)
-    ;;
-*linux*)
-    [[ -f /etc/os-release ]] || {
-      echo "Unknown Linux (/etc/os-release does not exist)"
-      exit 1
-    }
-    # shellcheck disable=SC1091
-    source /etc/os-release
+source $(dirname "${BASH_SOURCE:-0}")/util.sh
 
-    case $ID in
-    debian)
-      sudo apt install --yes locales
-      sudo sed -i -e '/ja_JP.UTF-8/s/^# *//' /etc/locale.gen
-      sudo locale-gen --keep-existing
-      ;;
-    *)
-      echo "Unsupported Linux: $ID / $PRETTY_NAME"
-      exit 1
-      ;;
-    esac
-    ;;
-*)
-    echo "Unsupported OS: $OSTYPE"
-    exit 1
-    ;;
-esac
+function install-locale-darwin()
+{
+  # do nothing
+}
+
+function install-locale-linux-debian()
+{
+  sudo apt install --yes locales
+  sudo sed -i -e '/ja_JP.UTF-8/s/^# *//' /etc/locale.gen
+  sudo locale-gen --keep-existing
+}
+
+install-locale-$(os)
