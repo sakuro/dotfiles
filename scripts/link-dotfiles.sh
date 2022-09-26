6ce8319215d449f16fab07db67c5bd7a24765776
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2022-09-26 14:50:26 +0900

    Use short options

--- scripts/link-dotfiles.sh
+++ scripts/link-dotfiles.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Don't use long command line options in this script.
+
 : "${DOTROOT:=$HOME/.dotfiles}"
 : "${DOTDEST:=$HOME}"
 
@@ -14,20 +16,20 @@ EXCLUDED_PATHSPECS=(
   ':!/.gitmodules'
 )
 
-[[ -d "$DOTDEST" ]] || mkdir --parents "$DOTDEST"
+[[ -d "$DOTDEST" ]] || mkdir -p "$DOTDEST"
 
 (cd "$DOTROOT" && git ls-files . "${EXCLUDED_PATHSPECS[@]}") | while read -r file; do
   from="$DOTROOT/${file}"
   to="$DOTDEST/${file}"
   dir="$(dirname "$file")"
 
-  [[ "$dir" = "." ]] || [[ -d "$DOTDEST/$dir" ]] || mkdir --parents "$DOTDEST/$dir"
-  [[ -e "$to" || -h "$to" ]] || ln --symbolic --verbose "$from" "$to"
+  [[ "$dir" = "." ]] || [[ -d "$DOTDEST/$dir" ]] || mkdir -p "$DOTDEST/$dir"
+  [[ -e "$to" || -h "$to" ]] || ln -sv "$from" "$to"
 done
 
 # Remove dangling symlinks
 for dir in $(cd "$DOTROOT" && find ./* -maxdepth 0 -type d); do
-  [[ -d $DOTDEST/$dir ]] && find "$DOTDEST/$dir" -xtype l -exec rm --verbose '{}' +
+  [[ -d $DOTDEST/$dir ]] && find "$DOTDEST/$dir" -xtype l -exec rm -v '{}' +
 done
 
 # Handle .config/git/include/credentials
@@ -35,12 +37,12 @@ CREDENTIAL_CONFIG_FILE="$DOTDEST/.config/git/include/credential"
 if [[ ! -f "$CREDENTIAL_CONFIG_FILE" ]]; then
   case "$OSTYPE" in
   darwin*)
-    ln --symbolic --verbose "$DOTROOT/.config/git/include/credential.darwin" "$CREDENTIAL_CONFIG_FILE"
+    ln -sv "$DOTROOT/.config/git/include/credential.darwin" "$CREDENTIAL_CONFIG_FILE"
     ;;
   linux*)
     if [[ -n "$WSL_DISTRO_NAME" ]]; then
       if [[ -x "/mnt/c/Program Files/Git/mingw64/libexec/git-core/git-credential-manager.exe" ]]; then
-        ln --symbolic --verbose "$DOTROOT/.config/git/include/credential.windows" "$CREDENTIAL_CONFIG_FILE"
+        ln -sv "$DOTROOT/.config/git/include/credential.windows" "$CREDENTIAL_CONFIG_FILE"
       fi
     fi
     ;;
