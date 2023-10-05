b5eeab2fab255001b39017631ee58b71434e6469
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2023-10-05 09:38:30 +0900

    Add hammerspoon setup

---
+++ scripts/setup-hammerspoon.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+defaults write org.hammerspoon.Hammerspoon MJConfigFile ~/.config/hammerspoon/init.lua
+defaults write org.hammerspoon.Hammerspoon input-methods '{"en" = "英字　　（ATOK）"; "ja" = "ひらがな（ATOK）";}'
