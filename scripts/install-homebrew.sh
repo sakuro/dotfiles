24ecbbf239a57e84fce604474e50116d13b13268
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2023-10-26 01:39:29 +0900

    Fix wrong detection of brew executable

--- scripts/install-homebrew.sh
+++ scripts/install-homebrew.sh
@@ -2,7 +2,7 @@
 
 eval "$(/usr/libexec/path_helper)"
 PATH=/opt/homebrew/bin:$PATH
-type -P brew > /dev/null && exit 0
+[[ -x /opt/homebrew/bin/brew ]] && exit 0
 bash -c "$(curl --fail --silent --show-error --location https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 type -P mas > /dev/null || brew install mas
--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -6,7 +6,7 @@ function install-packages-darwin()
 {
   eval "$(/usr/libexec/path_helper)"
   PATH=/opt/homebrew/bin:$PATH
-  type -P brew > /dev/null || scripts/install-homebrew.sh
+  [[ -x /opt/homebrew/bin/brew ]] || scripts/install-homebrew.sh
   brew bundle --no-lock
   chmod go-w "$(brew --prefix)/share"
 }
