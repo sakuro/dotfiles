1645a280b62ae9326d8a8c165f7d96bb00fbe63c
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2023-10-31 13:56:28 +0900

    Convert to Lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,7 @@
+vim.opt.ambiwidth = "double"
+vim.opt.autoindent = false
+vim.opt.modeline = true
+vim.opt.paste = true -- to make paste work as expected
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.viminfo = ''
