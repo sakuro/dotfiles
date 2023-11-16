2989b004faadd153b7de0761da65386b2adfdf07
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2023-11-16 13:28:28 +0900

    Prefer visual bell

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,4 +10,15 @@ config.font_size = 24
 config.window_background_opacity = 0.9
 config.enable_tab_bar = false
 
+config.audible_bell = 'Disabled'
+config.visual_bell = {
+    fade_in_function = 'EaseIn',
+    fade_in_duration_ms = 50,
+    fade_out_function = 'EaseOut',
+    fade_out_duration_ms = 50
+}
+config.colors = {
+    visual_bell = '#ffffff'
+}
+
 return config
