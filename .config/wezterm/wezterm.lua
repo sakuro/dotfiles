3c24940b9dc47463fb4b11f32ec4e6a2a3136412
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2023-11-16 13:30:27 +0900

    Change font size

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Nord (Gogh)'
 
 config.font = wezterm.font('Cica')
-config.font_size = 24
+config.font_size = 24.0
 
 config.window_background_opacity = 0.9
 config.enable_tab_bar = false
