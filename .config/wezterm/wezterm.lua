32b4c1584710347c446d6085119a3246fe731f5b
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2023-11-16 14:51:35 +0900

    Add next/prev pane traversal

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -19,13 +19,15 @@ config.colors = { visual_bell = '#ffffff' }
 
 config.leader = { key = 't', mods = 'CTRL', timeout_milliseconds = 1000 }
 config.keys = {
-    { key = 't', mods = 'LEADER|CTRL',  action = wezterm.action.SendKey { key = 't', mods = 'CTRL' } },
-    { key = '|', mods = 'LEADER|SHIFT', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
-    { key = '-', mods = 'LEADER',       action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
-    { key = 'h', mods = 'LEADER',       action = wezterm.action.ActivatePaneDirection 'Left' },
-    { key = 'j', mods = 'LEADER',       action = wezterm.action.ActivatePaneDirection 'Down' },
-    { key = 'k', mods = 'LEADER',       action = wezterm.action.ActivatePaneDirection 'Up' },
-    { key = 'l',  mods = 'LEADER',      action = wezterm.action.ActivatePaneDirection 'Right' },
+    { key = 't',   mods = 'LEADER|CTRL',  action = wezterm.action.SendKey { key = 't', mods = 'CTRL' } },
+    { key = '|',   mods = 'LEADER|SHIFT', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+    { key = '-',   mods = 'LEADER',       action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
+    { key = 'h',   mods = 'LEADER',       action = wezterm.action.ActivatePaneDirection 'Left' },
+    { key = 'j',   mods = 'LEADER',       action = wezterm.action.ActivatePaneDirection 'Down' },
+    { key = 'k',   mods = 'LEADER',       action = wezterm.action.ActivatePaneDirection 'Up' },
+    { key = 'l',   mods = 'LEADER',       action = wezterm.action.ActivatePaneDirection 'Right' },
+    { key = 'Tab', mods = 'LEADER',       action = wezterm.action.ActivatePaneDirection 'Next' },
+    { key = 'Tab', mods = 'LEADER|SHIFT',       action = wezterm.action.ActivatePaneDirection 'Prev' },
 }
 
 return config
