f7818eb0bfbe7546beea5cf0981dbbda182d20a8
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2024-05-14 09:59:53 +0900

    Clean default hammerspoon config directory

--- scripts/setup-hammerspoon.sh
+++ scripts/setup-hammerspoon.sh
@@ -2,3 +2,11 @@
 
 defaults write org.hammerspoon.Hammerspoon MJConfigFile ~/.config/hammerspoon/init.lua
 defaults write org.hammerspoon.Hammerspoon input-methods '{"en" = "英字　　（ATOK）"; "ja" = "ひらがな（ATOK）";}'
+
+if [[ -f $HOME/.hammerspoon/init.lua ]]; then
+  rm --verbose $HOME/.hammerspoon/init.lua
+fi
+
+if [[ -d $HOME/.hammerspoon ]]; then
+  rmdir --verbose --parents $HOME/.hammerspoon
+fi
