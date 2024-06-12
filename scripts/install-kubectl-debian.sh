df7e9b407064309423866547b7b312d83fba5e1d
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2024-06-12 15:42:31 +0900

    Add kubecl installer for debian

---
+++ scripts/install-kubectl-debian.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl
+sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list
+sudo apt update
+sudo apt install -y kubectl
