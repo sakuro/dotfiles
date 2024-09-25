33beb17578020ab4a2dc3aad39e319653a165f81
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2024-09-25 19:44:31 +0900

    Add mise installer for debian

---
+++ scripts/install-mise-debian.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo install -dm 755 /etc/apt/keyrings
+curl -s https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1> /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
+sudo apt update
+sudo apt install -y mise
