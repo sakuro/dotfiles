74e2ed411c1b21a622fd356a3a855f9c37812085
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2025-04-07 18:15:56 +0900

    :hammer: Refactor debian

--- scripts/install-mise-debian.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo install -dm 755 /etc/apt/keyrings
-curl -s https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1> /dev/null
-echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
-sudo apt update
-sudo apt install -y mise
--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -19,15 +19,45 @@ function install-packages-linux-debian()
   sudo apt install --yes $(cat apt-packages.txt)
 }
 
+funciton install-signing-key()
+{
+  local name="$1"
+  local url="$2"
+  local key_file="/usr/share/keyrings/$name.gpg"
+
+  [[ -f "$key_file" ]] || curl -fsSL "$url" | sudo gpg --dearmor -o "$key_file"
+}
+
+function install-apt-line()
+{
+  local name="$1"
+  local url="$2"
+  local apt_file="/etc/apt/sources.list.d/$name.list"
+  local key_file="/usr/share/keyrings/$name.gpg"
+  local arch="$(dpkg --print-architecture)"
+
+  [[ -f "$apt_file" ]] || echo "deb [arch=$arch signed-by=$key_file] $url stable main" | sudo tee "$apt_file"
+}
+
 function install-source-list()
 {
+  sudo apt update
+  sudo apt install -y apt-transport-https ca-certificates curl
+
+  sudo install -dm 755 /usr/share/keyrings
+  sudo install -dm 755 /etc/apt/sources.list.d
+
   # GitHub CLI
-  [[ -f /usr/share/keyrings/githubcli-archive-keyring.gpg ]] || {
-    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-  }
-  [[ -f /etc/apt/sources.list.d/github-cli.list ]] || {
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-  }
+  install-signing-key "github-cli" "https://cli.github.com/packages/githubcli-archive-keyring.gpg"
+  install-apt-line "github-cli" "https://cli.github.com/packages"
+
+  # mise-en-place
+  install-signing-key "mise" "https://mise.jdx.dev/gpg-key.pub"
+  install-apt-line "mise" "https://mise.jdx.dev/deb"
+
+  # kubectl
+  install-signing-key "kubernetes" "https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key"
+  install-apt-line "kubernetes" "https://pkgs.k8s.io/core:/stable:/v1.30/deb/"
 }
 
 install-packages-$(os)
