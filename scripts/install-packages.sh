379eae2b1cd621f5c9be48d7d49baf8c5ad6b1e8
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2025-06-08 17:59:46 +0900

    Remove additional source list

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -13,51 +13,8 @@ function install-packages-darwin()
 
 function install-packages-linux-debian()
 {
-  install-source-list
-  sudo apt update
   # shellcheck disable=SC2046
   sudo apt install --yes $(cat apt-packages.txt)
-}
-
-function install-signing-key()
-{
-  local name="$1"
-  local url="$2"
-  local key_file="/usr/share/keyrings/$name.gpg"
-
-  [[ -f "$key_file" ]] || curl -fsSL "$url" | sudo gpg --dearmor -o "$key_file"
-}
-
-function install-apt-line()
-{
-  local name="$1"
-  local url="$2"
-  local apt_file="/etc/apt/sources.list.d/$name.list"
-  local key_file="/usr/share/keyrings/$name.gpg"
-  local arch="$(dpkg --print-architecture)"
-
-  [[ -f "$apt_file" ]] || echo "deb [arch=$arch signed-by=$key_file] $url stable main" | sudo tee "$apt_file"
-}
-
-function install-source-list()
-{
-  sudo apt update
-  sudo apt install -y apt-transport-https ca-certificates curl
-
-  sudo install -dm 755 /usr/share/keyrings
-  sudo install -dm 755 /etc/apt/sources.list.d
-
-  # GitHub CLI
-  install-signing-key "github-cli" "https://cli.github.com/packages/githubcli-archive-keyring.gpg"
-  install-apt-line "github-cli" "https://cli.github.com/packages"
-
-  # mise-en-place
-  install-signing-key "mise" "https://mise.jdx.dev/gpg-key.pub"
-  install-apt-line "mise" "https://mise.jdx.dev/deb"
-
-  # kubectl
-  install-signing-key "kubernetes" "https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key"
-  install-apt-line "kubernetes" "https://pkgs.k8s.io/core:/stable:/v1.30/deb/"
 }
 
 install-packages-$(os)
