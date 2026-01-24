74b91aa36a118b1e8afc19f263d38325d20a5279
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-01-24 09:33:45 +0900

    :wrench: Workaround for mise hooks

--- .config/git/hooks/support.zsh
+++ .config/git/hooks/support.zsh
@@ -6,5 +6,8 @@ function detect-languages() {
 
 # Enable tools managed by mise if mise is executable
 if (( $+commands[mise] )); then
+  # mise tries removing _mise_hook_precmd from hooks, let it work with set -u
+  typeset -ag chpwd_functions
+  typeset -ag precmd_functions
   eval "$(mise activate zsh)"
 fi
