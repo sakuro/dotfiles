cbbb6be14e01a2b890f47bcf28bc31ed04713cbe
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-04-05 17:17:35 +0900

    :art: Quote variables in install-packages.sh

--- scripts/linux-debian/install-packages.sh
+++ scripts/linux-debian/install-packages.sh
@@ -13,11 +13,11 @@ AQUA_INSTALLER_VERSION=v3.1.2
 AQUA_INSTALLER_URL=https://raw.githubusercontent.com/aquaproj/aqua-installer/${AQUA_INSTALLER_VERSION}/aqua-installer
 
 aqua_installer_tmp_dir=$(mktemp -d)
-trap "test -d $aqua_installer_tmp_dir && rm -rf $aqua_installer_tmp_dir" EXIT
+trap 'test -d "$aqua_installer_tmp_dir" && rm -rf "$aqua_installer_tmp_dir"' EXIT
 
-cd $aqua_installer_tmp_dir
+cd "$aqua_installer_tmp_dir"
 
-curl -sSfLO ${AQUA_INSTALLER_URL}
+curl -sSfLO "${AQUA_INSTALLER_URL}"
 echo "9a5afb16da7191fbbc0c0240a67e79eecb0f765697ace74c70421377c99f0423  aqua-installer" | sha256sum -c -
 chmod +x aqua-installer
 ./aqua-installer
