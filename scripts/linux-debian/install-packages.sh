2fb1b22aa2863a3a48f4ca2e5f4d33c1cc27e254
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-08-06 23:26:40 +0900

    :sparkles: Install packages via mise bootstrap on Debian

--- scripts/linux-debian/install-packages.sh
+++ scripts/linux-debian/install-packages.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-# shellcheck disable=SC2046
-sudo apt install --yes $(cat files/apt-packages.txt)
+if ! command -v mise >/dev/null 2>&1; then
+  curl https://mise.run | sh
+fi
+
+PATH=$HOME/.local/bin:$PATH
+MISE_AUTO_ENV=true mise bootstrap packages apply --yes
 
 sudo apt install --yes locales
 sudo sed -i -e '/ja_JP.UTF-8/s/^# *//' /etc/locale.gen
 sudo locale-gen --keep-existing
 
-if command -V mise >/dev/null 2>&1; then
-  echo "mise is already installed"
-  exit 0
-fi
-
-curl https://mise.run | sh
-
-PATH=$HOME/.local/bin:$PATH
 mise install
