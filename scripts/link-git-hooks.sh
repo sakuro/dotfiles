f4372cbb5e15836508eba14e900a14ae8892be78
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-08-19 18:41:35 +0900

    :recycle: Use mise' dotfiles linking feature

--- scripts/link-git-hooks.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ -f $HOME/.dotfiles/.git/hooks/post-merge ]]; then
-  exit 0
-fi
-
-mkdir -p $HOME/.dotfiles/.git/hooks
-ln -s -v "$HOME/.dotfiles/scripts/post-merge" "$HOME/.dotfiles/.git/hooks/post-merge"
