7c5243f3b0146d0b7ea6e6e4d06fb162eb38da4f
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-08-19 10:20:53 +0900

    :wrench: bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
-BOOTSTRAP_TOML_URL="https://dot.2238.club/.config/mise/bootstrap.toml"
+set -euo pipefail
+
+BOOTSTRAP_TOML_URL="https://dot.2238.club/.config/mise/config.toml"
 DOTROOT="$HOME/.dotfiles"
 PATH="$HOME/bin:$PATH"
 
-set -euo pipefail
+case "$(uname)" in
+Darwin) OS=macos ;;
+Linux) OS=linux ;;
+*) echo "Unsupported OS"; exit 1 ;;
+esac
 
 curl -fsSL https://mise.run | MISE_INSTALL_PATH="$HOME/bin/mise" bash
 eval "$(mise activate)"
 
-bootstrap_config="$(mktemp)"
-trap 'rm -f "$bootstrap_config"' EXIT
+bootstrap_config_dir="$(mktemp -d)"
+trap 'rm -rf "$bootstrap_config_dir"' EXIT
 
-curl -fsSL -o "$bootstrap_config" "${BOOTSTRAP_TOML_URL}"
-MISE_GLOBAL_CONFIG_FILE="$bootstrap_config" mise bootstrap repos apply
-
-#$DOTROOT/scripts/link-dotfiles.sh"
-#mise bootstrap apply
+curl -fsSL -o "$bootstrap_config_dir/mise.toml" "${BOOTSTRAP_TOML_URL}"
+curl -fsSL -o  "$bootstrap_config_dir/mise.${OS}.toml" "${BOOTSTRAP_TOML_URL/.toml/.${OS}.toml}"
+MISE_AUTO_ENV=true mise bootstrap --yes
