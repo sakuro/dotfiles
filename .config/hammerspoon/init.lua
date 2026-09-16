4e25f573e19341580497f7940c2ab9d3ce7c453e
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-09-16 14:09:40 +0900

    :fire: Disable IM toggle by cmd

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -74,7 +74,7 @@ switchToEnglishOnActivation = hs.application.watcher.new(function(name, event, a
   end
 end)
 
-switchInputMethodByCommandKey:start()
+-- switchInputMethodByCommandKey:start()
 switchToEnglishOnActivation:start()
 
 -- Audio volume management
